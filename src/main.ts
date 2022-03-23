43b356af6ff1a8bb079425f5099444378ac4c44d
alex parfenov <alexp-2004@mail.ru>
2022-03-23 13:34:23 +0700

    add logger and src

---
+++ src/main.ts
@@ -0,0 +1,9 @@
+import { LoggerService } from './logger/logger.service';
+import { App } from './app';
+
+async function bootstrap() {
+  const app = new App(new LoggerService());
+  await app.init();
+}
+
+bootstrap();
